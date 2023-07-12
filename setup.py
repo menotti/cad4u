430a3ff5e7fb00410b8316cbdab6d4f96f93d192
Ricardo Menotti <menotti@gmail.com>
2023-07-12 08:26:39 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author='Michael Canesche',
     author_email='michael.canesche@gmail.com',
     py_modules=['plugin', 'cuda.cuda', 'c.c', 'cpp.cpp', 'verilog.verilog', 'java.java', 'gem5.gem5', 'valgrind.valgrind', 'llvm.llvm', 'rust.rust', 'common.helper', 'common.tool'],
-    url='https://github.com/lesc-ufv/cad4u',
+    url='https://github.com/menotti/cad4u',
     license='LICENSE',
     description='Jupyter notebook plugin to run CUDA, C/C++, GCC code, Verilog, LLVM, Gem5',
     # long_description=open('README.md').read(),
